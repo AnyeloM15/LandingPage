db43ab1dcbd4a340467f27480e397570915aeaca
AnyeloM15 <022000949@upsrj.edu.mx>
2024-10-09 09:43:59 -0600

    Eliminados archivos que no se utilizaron, y ajustes en los pequenos scripts by Miguel

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@
     new WOW().init();
 
 
-    // Fixed Navbar
+    // Navbar
     $(window).scroll(function () {
         if ($(window).width() < 992) {
             if ($(this).scrollTop() > 45) {
@@ -29,51 +29,6 @@
                 $('.fixed-top').addClass('bg-dark shadow').css('top', -45);
             } else {
                 $('.fixed-top').removeClass('bg-dark shadow').css('top', 0);
-            }
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-    // Causes progress
-    $('.causes-progress').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1000,
-        center: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
             }
         }
     });
